a87e81db39c1a0b25bf2be54aaf21b134bf2576f
davidsetyaahr <davidsetya24@gmail.com>
2019-11-26 11:09:22 +0700

    pages

---
+++ pages/contact.php
@@ -0,0 +1,41 @@
+<?php
+$title = "Jalanin | Packages";
+$top['first'] = "Jalanin";
+$top['second'] = "Kontak Kami";
+include "../common/top.php";
+ ?>
+<div class="container custom klas">
+<center>
+  
+<p class="help">Ada yang bisa kami bantu?</p>
+<h5>Hubungi kami untuk info berlebih. Pelayanan terbaik berusaha kami berikan untuk anda
+<br>
+</h5>
+<div class="container view">
+<div class="row">
+<div class="col-md-2 col-md-offset-5">
+ <p class="hr">Lihat Map</p>
+</div>
+</div>
+</div>
+<div class="container">
+  <div class="row">
+    <div class="col-md-4 garis">
+        <h4 class="kon">Kontak Kami </h4>
+        <p>info@jalanin.com <br>+62 823 139 728  / <br>0332-2345-221</p>
+    </div>
+    <div class="col-md-4 garis">
+      <h4 class="kon">Alamat </h4>
+      <p>Jalan Mastrip, Perum <br>Mastrip Blok R/R6<br>Jember</p>
+    </div>
+    <div class="col-md-4">
+      <h4 class="kon">Jam Kerja</h4>
+      <p>Mon-Sat: 6am - 7pm<br>Sun : 8am - 7pm</p>
+    </div>
+  </div>
+</div>
+</center>
+  </div>
+ <?php
+  include "../common/bottom.php";
+ ?>
